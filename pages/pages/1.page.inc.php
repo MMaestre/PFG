eb78ed41bf380b5fff6890c19517e8947f4a548b
MMaestre <airam.maestre@gmail.com>
2016-06-12 12:01:20 +0200

    page 1
    Home

---
+++ pages/pages/1.page.inc.php
@@ -0,0 +1,5 @@
+<h1 class="theme-heading">Organiza tu tarde de cine</h1>
+                        <p class="lead">Dinos a qué hora quieres empezar, cuántos cines puedes visitar y tu medio de transporte y te ayudamos a preparar la tarde perfecta</p>
+                        <p class="lead">
+                            <a href="?page=2" class="btn btn-lg btn-default">Empieza ahora</a>
+                        </p>
